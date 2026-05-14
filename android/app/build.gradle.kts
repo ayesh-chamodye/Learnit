003203a3fffe53594ebd11f372316c20130322d0
Ayesh Chamodye <ayeshchamodye@gmail.com>
2026-05-14 20:10:16 +0530

    libso missing fixed

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -28,8 +28,7 @@ android {
         versionCode = flutter.versionCode
         versionName = flutter.versionName
         ndk {
-            // Use SYMBOL_TABLE to keep minimal debug symbols and avoid strip errors
-            debugSymbolLevel = "SYMBOL_TABLE"
+            abiFilters 'armeabi-v7a', 'arm64-v8a', 'x86_64'
         }
     }
 
